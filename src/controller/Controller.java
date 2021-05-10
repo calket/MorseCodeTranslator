ec38b7452b01751d45c40b53ac6ef967eca8ba2b
calket <github@calket.de>
2021-05-10 13:06:14 +0200

    Dissection of morse code and translation works in console

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,15 +1,51 @@
 package controller;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class Controller {
-
-    String morse;
     public TextField input;
+    private String spaceString = " ";
+    private String slashString ="/";
+    private char space = spaceString.charAt(0);
+    private char slash = slashString.charAt(0);
+    private String decoded = "";
 
-    public void handleTransButton(){
-        morse = input.getText();
-        System.out.println(morse);
+    private static final char[] letters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    private static final String[] morseCode = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",
+            ".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+    @FXML
+    private void handleTransButton(){
+        decoded = "";
+        String code = input.getText() + " ";
+        dissection(code);
+    }
+
+    private void dissection(String code) {
+        String codeLetter = "";
+
+        for( char c : code.toCharArray()){
+            if( c != slash ){
+                if (c != space){
+                    codeLetter = codeLetter+c;
+                }else{
+                    translateLetter(codeLetter);
+                    codeLetter ="";
+                }
+            }else {
+                decoded = decoded+" ";
+            }
+        }
+    }
+
+    private void translateLetter(String codeLetter) {
+        for (int i = 0; i < letters.length; i++) {
+            if( codeLetter.equals(morseCode[i])){
+                decoded = decoded + letters[i];
+            }
+        }
+        System.out.println(decoded);
     }
 
 }
